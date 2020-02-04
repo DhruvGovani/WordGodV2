6eb643d0edcd055eb8c47ff3188f1ab88fc3cb51
Govani Dhruv Vijaykumar <37019716+DhruvGovani@users.noreply.github.com>
2020-02-04 19:30:56 +0530

    Add files via upload

---
+++ wordGodV2.py
@@ -0,0 +1,312 @@
+import os
+import random
+
+#intro
+#------------------------------IGNORE THIS CODE--------------------------------------
+os.system("clear")
+def printPattern():
+    patterRand = random.randint(0,3)
+
+
+    if patterRand == 0:
+
+     print("#################################################")
+     print("#################################################")
+     print("############### Word God V2 #####################")
+     print("############# BY DHRUV GOVANI ###################")
+     print("#################################################")
+     print("#################################################\n")
+
+    elif patterRand == 1:
+        print("███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█")
+        print("████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█")
+        print("███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("███▓█▓██WORD██▓███▓╬╬╬╬╬╬▓██GOD██▓╬╬╬╬▓█")
+        print("████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█")
+        print("███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█")
+        print("█████▓▓B▓Y▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██")
+        print("█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██")
+        print("████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██")
+        print("████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██")
+        print("█████▓███▓▓▓▓▓▓▓▓DHRUV▓GOVANI╬█▓╬╬╬╬╬▓██")
+        print("█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███")
+        print("██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███")
+        print("███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████")
+        print("███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████")
+        print("████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████")
+        print("█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████")
+        print("██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████")
+        print("███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████")
+        print("██████████████▓▓▓███V2╬╬╬╬╬╬╬╬██████████")
+        print("███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████")
+
+    elif patterRand == 2:
+        print("\t  ──────▄▀▄─────▄▀▄")
+        print("\t  ─────▄█░░▀▀▀▀▀░░█▄")
+        print("\t  ─▄▄──█░░░░░░░░░░░█──▄▄")
+        print("\t  █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█")
+        print("▂▃▄▅▆▇█▓▒░░W░O░R░D░ ░G░O░D░ ░V░2░░▒▓█▇▆▅▄▃▂")
+        print("▂▃▄▅▆▇█▓▒░░░♥ BY DHRUV GOVANI ♥░░░░▒▓█▇▆▅▄▃▂")
+        print("●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●")
+
+    else:
+        print("MOʁDҼODΛ5B⅄DHʁꓵΛҼOΛⱯИIIИAVOӘVUЯHꓷYꓭςVꓷOӘꓷЯOW")
+        print("IИAVOӘVUЯHꓷYꓭςVꓷOӘꓷЯOWꓭՇꓥꓷOꓨꓷꓤOMMOʁDҼODΛ5B⅄D")
+        print("MOʁD5B⅄ꓵΛⱯИ ★ ᙎOᖇᗪ ᘜOᗪ ᐯ2 ★ ИAVOӘVHꓷYꓷЯOWOʁDҼ")
+        print("MOʁDҼꓵOΛИI☣ ᙖY ᗪᕼᖇᙀᐯ ᘜOᐯᗩᑎI ☣ OЯYꓭςVꓷOʁDҼЯOW")
+        print("INⱯꓥOꓨꓥꓵꓤHꓷ⅄ꓭՇꓥꓷOꓨꓷꓤOMINAVOGVURHDYB2VDOGDROW")
+        print("INAVOGVURHDYB2VDOGDROWMOʁDҼODΛ5B⅄DHʁꓵΛҼOΛⱯИI")
+
+
+printPattern()
+print("\nFeatures :\n \n1. Read Words from two files and makes combination of them \n2. You can add a special word or character to combine thim with all possible combinations \n3. Removes Duplicate Words \n4. Writes All combinations to the destination file defined by user \n5. Display Stats")
+
+print("\nUSAGE:\n \n1. MUST CHECK FOR FILES FOR SAME DIRECTORY AS WORD GOD 2 \n2. MUST NAME ALL FILES WITH EXTENSIONS \n3. IF DESTINATION FILE IS ALREADY EXIST THEN IT WILL BE OVERWRITTEN \n")
+
+
+
+#---------------------------- code starts from here----------------------------------
+
+
+
+#init
+
+FileData1 = []
+FileData2 = []
+
+
+#userChoices
+
+sourceFile1 = input("\nEnter Name of the File 1 with extension: ")
+sourceFile2 = input("\nEnter Name of the File 2 with extension: ")
+
+#get files
+
+for word in open(sourceFile1).read().split():
+    FileData1.append(word)
+    
+for word in open(sourceFile2).read().split():
+    FileData2.append(word)
+    
+
+
+#make combos
+
+words = []
+wordsDuplicate = []
+wordsCombo = []
+
+for i in range(len(FileData1)):
+    words.append(FileData1[i])
+    
+for i in range(len(FileData2)):
+    words.append(FileData2[i])
+    
+for i in range(len(words)):
+    wordsDuplicate.append(words[i])
+    
+for i in range(len(words)):
+    wordsCombo.append(words[i] + wordsDuplicate[i])
+
+
+FetchedWords = len(words)
+
+for j in range(len(words)):
+    
+ for i in range(len(words)):
+    #main logic
+     wordsCombo.append(words[j] + wordsDuplicate[i])
+     wordsCombo.append(words[i] + wordsDuplicate[j])
+     
+    #capitalize logic
+     wordsCombo.append(words[j].capitalize() + wordsDuplicate[i])
+     wordsCombo.append(words[j].capitalize() + wordsDuplicate[i].capitalize())
+     wordsCombo.append(words[j] + wordsDuplicate[i].capitalize())
+     wordsCombo.append(words[i].capitalize() + wordsDuplicate[j].capitalize())
+     wordsCombo.append(words[i].capitalize() + wordsDuplicate[j])
+     wordsCombo.append(words[i] + wordsDuplicate[j].capitalize())
+     
+    #upper logic
+     wordsCombo.append(words[j].upper() + wordsDuplicate[i])
+     wordsCombo.append(words[j].upper() + wordsDuplicate[i].upper())
+     wordsCombo.append(words[j] + wordsDuplicate[i].upper())
+     wordsCombo.append(words[i].upper() + wordsDuplicate[j].upper())
+     wordsCombo.append(words[i].upper() + wordsDuplicate[j])
+     wordsCombo.append(words[i] + wordsDuplicate[j].upper())
+    
+    
+    #lower logic
+     wordsCombo.append(words[j].lower() + wordsDuplicate[i])
+     wordsCombo.append(words[j].lower() + wordsDuplicate[i].lower())
+     wordsCombo.append(words[j] + wordsDuplicate[i].lower())
+     wordsCombo.append(words[i].lower() + wordsDuplicate[j].lower())
+     wordsCombo.append(words[i].lower() + wordsDuplicate[j])
+     wordsCombo.append(words[i] + wordsDuplicate[j].lower())
+    
+
+#add special char
+specialCharCount = 0
+usedSpecialChar = []
+
+speCharChoice = input("\nWould You Like to add a special charachter? (Y/N) : ")
+
+
+while speCharChoice == "Y":
+ specialCharCount += 1
+ speChar = input("\nEnter a Special Character or Word or Number you want : ")
+ usedSpecialChar.append(speChar)
+ for j in range(len(words)):
+    
+     for i in range(len(words)):
+        ####PRE POS OF SPECIAL CHAR####
+        #main logic
+         wordsCombo.append(speChar + words[j] + wordsDuplicate[i])
+         wordsCombo.append(speChar + words[i] + wordsDuplicate[j])
+         
+        #capitalize logic
+         wordsCombo.append(speChar + words[j].capitalize() + wordsDuplicate[i])
+         wordsCombo.append(speChar + words[j].capitalize() + wordsDuplicate[i].capitalize())
+         wordsCombo.append(speChar + words[j] + wordsDuplicate[i].capitalize())
+         wordsCombo.append(speChar + words[i].capitalize() + wordsDuplicate[j].capitalize())
+         wordsCombo.append(speChar + words[i].capitalize() + wordsDuplicate[j])
+         wordsCombo.append(speChar + words[i] + wordsDuplicate[j].capitalize())
+         
+        #upper logic
+         wordsCombo.append(speChar + words[j].upper() + wordsDuplicate[i])
+         wordsCombo.append(speChar + words[j].upper() + wordsDuplicate[i].upper())
+         wordsCombo.append(speChar + words[j] + wordsDuplicate[i].upper())
+         wordsCombo.append(speChar + words[i].upper() + wordsDuplicate[j].upper())
+         wordsCombo.append(speChar + words[i].upper() + wordsDuplicate[j])
+         wordsCombo.append(speChar + words[i] + wordsDuplicate[j].upper())
+        
+        
+        #lower logic
+         wordsCombo.append(speChar + words[j].lower() + wordsDuplicate[i])
+         wordsCombo.append(speChar + words[j].lower() + wordsDuplicate[i].lower())
+         wordsCombo.append(speChar + words[j] + wordsDuplicate[i].lower())
+         wordsCombo.append(speChar + words[i].lower() + wordsDuplicate[j].lower())
+         wordsCombo.append(speChar + words[i].lower() + wordsDuplicate[j])
+         wordsCombo.append(speChar + words[i] + wordsDuplicate[j].lower())
+
+         #########POST POS#############
+         #main logic
+        #main logic
+         wordsCombo.append(words[j] + wordsDuplicate[i] + speChar)
+         wordsCombo.append(words[i] + wordsDuplicate[j] + speChar)
+         
+        #capitalize logic
+         wordsCombo.append(words[j].capitalize() + wordsDuplicate[i] + speChar)
+         wordsCombo.append(words[j].capitalize() + wordsDuplicate[i].capitalize() + speChar)
+         wordsCombo.append(words[j] + wordsDuplicate[i].capitalize() + speChar)
+         wordsCombo.append(words[i].capitalize() + wordsDuplicate[j].capitalize() + speChar)
+         wordsCombo.append(words[i].capitalize() + wordsDuplicate[j] + speChar)
+         wordsCombo.append(words[i] + wordsDuplicate[j].capitalize() + speChar)
+         
+        #upper logic
+         wordsCombo.append(words[j].upper() + wordsDuplicate[i] + speChar)
+         wordsCombo.append(words[j].upper() + wordsDuplicate[i].upper() + speChar)
+         wordsCombo.append(words[j] + wordsDuplicate[i].upper() + speChar)
+         wordsCombo.append(words[i].upper() + wordsDuplicate[j].upper() + speChar)
+         wordsCombo.append(words[i].upper() + wordsDuplicate[j] + speChar)
+         wordsCombo.append(words[i] + wordsDuplicate[j].upper() + speChar)
+        
+        
+        #lower logic
+         wordsCombo.append(words[j].lower() + wordsDuplicate[i] + speChar)
+         wordsCombo.append(words[j].lower() + wordsDuplicate[i].lower() + speChar)
+         wordsCombo.append(words[j] + wordsDuplicate[i].lower() + speChar)
+         wordsCombo.append(words[i].lower() + wordsDuplicate[j].lower() + speChar)
+         wordsCombo.append(words[i].lower() + wordsDuplicate[j] + speChar)
+         wordsCombo.append(words[i] + wordsDuplicate[j].lower() + speChar)
+         
+         ##########MIDDLE POS#########
+         
+         wordsCombo.append(words[j] + speChar + wordsDuplicate[i])
+         wordsCombo.append(words[i] + speChar + wordsDuplicate[j])
+         
+        #capitalize logic
+         wordsCombo.append(words[j].capitalize() + speChar + wordsDuplicate[i])
+         wordsCombo.append(words[j].capitalize() + speChar + wordsDuplicate[i].capitalize())
+         wordsCombo.append(words[j] + speChar + wordsDuplicate[i].capitalize())
+         wordsCombo.append(words[i].capitalize() + speChar + wordsDuplicate[j].capitalize())
+         wordsCombo.append(words[i].capitalize() + speChar + wordsDuplicate[j])
+         wordsCombo.append(words[i] + speChar + wordsDuplicate[j].capitalize())
+         
+        #upper logic
+         wordsCombo.append(words[j].upper() + speChar + wordsDuplicate[i])
+         wordsCombo.append(words[j].upper() + speChar + wordsDuplicate[i].upper())
+         wordsCombo.append(words[j] + speChar + wordsDuplicate[i].upper())
+         wordsCombo.append(words[i].upper() + speChar + wordsDuplicate[j].upper())
+         wordsCombo.append(words[i].upper() + speChar + wordsDuplicate[j])
+         wordsCombo.append(words[i] + speChar + wordsDuplicate[j].upper())
+        
+        
+        #lower logic
+         wordsCombo.append(words[j].lower() + speChar + wordsDuplicate[i])
+         wordsCombo.append(words[j].lower() + speChar + wordsDuplicate[i].lower())
+         wordsCombo.append(words[j] + speChar + wordsDuplicate[i].lower())
+         wordsCombo.append(words[i].lower() + speChar + wordsDuplicate[j].lower())
+         wordsCombo.append(words[i].lower() + speChar + wordsDuplicate[j])
+         wordsCombo.append(words[i] + speChar + wordsDuplicate[j].lower())
+         
+ speCharChoice = input("\nWould You Like to add a special charachter? (Y/N) : ")
+
+#duplicate remover
+totalgenratedWords = len(wordsCombo)
+dupliChoice = input("\n#{} Passwords Are Genrated Would You Like to remove Duplicate Words? (Y/N) :".format(totalgenratedWords))
+
+if dupliChoice == "Y":
+ wordsCombo = list( dict.fromkeys(wordsCombo) )
+ print("\nDuplicate Words Removed")
+else:
+ print("")
+
+afterDupRemoval = len(wordsCombo)
+
+
+#store in destination file
+
+desFileName = input("\nEnter Name of Destination file with extension: ")
+desfile = open(desFileName,"w+")
+
+for i in range(len(wordsCombo)):
+    desfile.write(wordsCombo[i] + '\n')
+    
+print("\nPasswords Written to file Succesfully")
+
+#printing Stats
+os.system("clear")
+printPattern()
+print("\n\t$$$ SUCCESSFUL $$$\n")
+print("\nNumber of Files Fetched : 2 \n\t Filename : {} \n\t Filename : {}".format(sourceFile1,sourceFile2))
+print("\nNumber of Files Written : 1 \n\t Filename : {}".format(desFileName))
+print("\nTotal Words Fetched From Files: {}".format(FetchedWords))
+print("\nNumber of Character Used : {}".format(specialCharCount))
+for i in range(len(usedSpecialChar)):
+ print("\tChar Number {} : {} ".format(i,usedSpecialChar[i]))
+print("\nTotal Words Genrated : {}".format(totalgenratedWords))
+print("\nRemoved Duplicate Word : {}".format(totalgenratedWords - afterDupRemoval))
+print("\nWords Combination Written to File : {}\n".format(len(wordsCombo)))
+    
+    
+
+################debug#################
+
+#Uncomment for debugging
+
+#print("#debug")
+#print(words)
+#print(wordsDuplicate)
+#print("#debug")
+#print("\n")
+#print(wordsCombo)
+
+
+#################################################
+############### Word God V2 #####################
+############# BY DHRUV GOVANI ###################
+#################################################
